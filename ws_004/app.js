6002e5c71cec46f07d57df1dde2ccc46f993aff9
jyanoss <jyanoss@gmail.com>
2022-06-14 10:28:17 +0900

    1111

--- ws_004/app.js
+++ ws_004/app.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const nunjucks = require('nunjucks');
-const dotenv = require('dotenv');
+const express       = require('express');
+const path          = require('path');
+const morgan        = require('morgan');
+const cookieParser  = require('cookie-parser');
+const session       = require('express-session');
+const nunjucks      = require('nunjucks');
+const dotenv        = require('dotenv');
 
 dotenv.config();
-const webSocket = require('./socket');
-const indexRouter = require('./routes');
+const webSocket     = require('./socket');
+const indexRouter   = require('./routes');
 
 const app = express();
 app.set('port', process.env.PORT || 30002);
