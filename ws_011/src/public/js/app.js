fef102be9979fb3669b34d803c8aa7c16a135199
jyanoss <jyanoss@gmail.com>
2022-06-23 18:23:51 +0900

    회의실 버젼 작업중

---
+++ ws_011/src/public/js/app.js
@@ -0,0 +1,248 @@
+// app.js
+
+const socket = io(); // io function은 알아서 socket.io를 실행하고 있는 서버를 찾을 것이다!
+
+// 방을 만들것!! (socket IO에는 이미 방기능이 있다!)
+const div_login_id  = document.getElementById("login_id");
+const div_room_name = document.getElementById("room_name");
+const btn_logout    = document.getElementById("btn_logout");
+const btn_leave     = document.getElementById("btn_leave");
+
+const login         = document.getElementById("login");
+const login_form    = login.querySelector("form");
+
+const lobby         = document.getElementById("lobby");
+const lobby_form    = lobby.querySelector("form");
+
+const room          = document.getElementById("room");
+const room_form     = room.querySelector("form");
+
+
+
+lobby.hidden        = true; 
+room.hidden         = true; 
+btn_logout.hidden   = true;
+
+let g_login_id;         //
+let g_room_name;        //
+
+
+//================================================================================================
+// 함수정의
+//================================================================================================
+
+// 로그인 버튼 클릭 (submit)
+function hdl_a000_login(event){
+    event.preventDefault();
+    const input = login_form.querySelector("input");
+    socket.emit( "a000_login",input.value); 
+    input.value = "";
+}
+
+// 로그인 결과처리
+function hdl_a000_login_result(ret_code, login_id){
+    if(ret_code === '0'){
+        //로비페이지 이동
+        login.hidden            = true;
+        lobby.hidden            = false;
+        room.hidden             = true; 
+        btn_logout.hidden       = false;
+
+        //로그인아이디 저장
+        g_login_id              = login_id;
+        //로그인텍스트 출력
+        div_login_id.innerText  = `[${login_id}] 으로 로그인 성공`;
+        //방이름텍스트 삭제
+        div_room_name.innerText = "";
+
+        btn_logout.addEventListener("click", hdl_a999_logout);        //로그아웃 버튼 이벤트 등록
+        lobby_form.addEventListener("submit", hdl_b000_roomCreate);   //방생성 이벤트 등록
+
+    }else{
+        login.hidden        = false;
+        lobby.hidden        = true;
+        room.hidden         = true; 
+        btn_logout.hidden   = true;
+
+        alert('로그인 실패');
+        g_login_id                  = "";
+        div_login_id.innerText      = "";
+        div_room_name.innerText     = "";
+        login_form.querySelector("input") = '';
+        return false;
+
+    }
+
+}
+
+// 로그아웃 버튼 클릭
+function hdl_a999_logout(event){
+    event.preventDefault();
+    socket.emit( "a999_logout"); 
+}
+
+// 로그아웃 결과처리
+function hdl_a999_logout_result(ret_code, msg){
+    if(ret_code === '0'){
+
+        alert(msg);
+        //로그인페이지 이동
+        login.hidden            = false;
+        lobby.hidden            = true;
+        room.hidden             = true; 
+        btn_logout.hidden       = true;
+        //로그인아이디 삭제
+        g_login_id              = "";
+        //로그인텍스트 삭제
+        div_login_id.innerText  = "";
+        div_room_name.innerText = "";
+
+    }else{
+        alert(msg);
+        return false;
+    }
+
+}
+
+// 방생성 버튼 클릭
+function hdl_b000_roomCreate(event){
+    event.preventDefault();
+    const input = lobby_form.querySelector("input");
+    socket.emit( "b000_roomCreate",input.value);     
+    input.value = "";
+}
+
+// 방생성 결과처리
+function hdl_b000_roomCreate_result(ret_code, room_name){
+    if(ret_code === '0'){
+
+        //회의실페이지 이동
+        login.hidden    = true;
+        lobby.hidden    = true;
+        room.hidden     = false; 
+
+        //const h3 = room.querySelector("h3");
+        //방이름 저장
+        g_room_name             = room_name
+        //방이름 출력
+        div_room_name.innerText = `Room ${room_name}`               
+
+        const msgForm = room.querySelector("#msg");
+        btn_leave.addEventListener("click", hdl_b999_roomLeave);    // 방나가기 버튼 이벤트 등록
+        msgForm.addEventListener("submit", hdl_c000_msgSend);       // 메시지전송 버튼 이벤트 등록
+
+    }else{
+        //alert(room_name);
+        return false;
+    }
+
+}
+
+// 방나가기 버튼 클릭
+function hdl_b999_roomLeave(event){
+    event.preventDefault();
+    var room_name = g_room_name;
+    socket.emit( "b999_roomLeave", room_name);   
+  
+}
+
+// 방나가기 결과처리
+function hdl_b999_roomLeave_result(ret_code, msg){
+
+    if(ret_code === '0'){
+
+        alert(msg);
+        //로비페이지 이동
+        login.hidden            = true;
+        lobby.hidden            = false;
+        room.hidden             = true; 
+        btn_logout.hidden       = false;
+
+        //방이름 삭제
+        g_room_name             = "";
+        //방이름 출력삭제
+        div_room_name.innerText = "";
+        //채팅창초기화처리
+        const ul = room.querySelector("ul");
+        while (ul.firstChild) {
+            ul.removeChild(ul.firstChild);
+        }
+
+    }else{
+        alert(msg);
+        return false;
+    }
+
+}
+
+// 메시지 버튼 클릭
+function hdl_c000_msgSend(event){
+    event.preventDefault();
+    
+    var room_name   = g_room_name;
+    var login_id    = g_login_id;
+    const input     = room_form.querySelector("input");
+    socket.emit("c000_msgSend", input.value, room_name, login_id);   
+    input.value     = ""; 
+}
+
+// 메시지 전송 결과처리
+function addMessage(message){
+    const ul = room.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerText = message;
+    ul.appendChild(li);
+}
+
+
+//================================================================================================
+// 이벤트등록
+//================================================================================================
+
+// 로그인버튼 클릭 이벤트 등록
+login_form.addEventListener("submit", hdl_a000_login);     //로그인이벤트등록
+
+
+//================================================================================================
+// 소켓정의
+//================================================================================================
+
+//테스트용 메시지
+socket.on("io_system_msg", (msg) => {  console.log("io_system_msg", msg);    })
+
+//로그인처리
+socket.on("a000_login_result", (ret_code, login_id) => {  
+    console.log("a000_login_result", login_id);
+    hdl_a000_login_result(ret_code, login_id);    
+})
+
+//로그인아웃처리
+socket.on("a999_logout_result", (ret_code, msg) => {  
+    console.log("a999_logout_result", ret_code, msg);
+    hdl_a999_logout_result(ret_code, msg);    
+})
+
+//방생성처리
+socket.on("b000_roomCreate_reuslt", (ret_code, room_name) => {  
+    console.log("b000_roomCreate_reuslt", ret_code, room_name);
+    hdl_b000_roomCreate_result(ret_code, room_name);    
+})
+
+//방나가기처리
+socket.on("b999_roomLeave_result", (ret_code, msg) => {  
+    console.log("b999_roomLeave_result", ret_code, msg);
+    hdl_b999_roomLeave_result(ret_code, msg);    
+ 
+})
+
+//메세지처리
+socket.on("c000_msgSend_return", (ret_code, msg) => { 
+    console.log(">>>>>>>>>>> c000_msgSend_return", ret_code, msg);
+    addMessage(msg);
+})
+
+
+
+
+
+
---
+++ ws_011/src/server.js
@@ -0,0 +1,99 @@
+// server.js
+
+import http from "http"; // 이미 기본 설치되어있음
+import WebSocket from "ws"; // 기본설치!
+import express from "express"; // npm i express 설치
+import { instrument } from "@socket.io/admin-ui";
+import { Server } from "socket.io"; 
+
+const app = express(); // app이라는 변수에 가져와서 사용
+
+app.set("view engine", "pug"); // 뷰 엔진을 pug로 하겠다
+app.set("views", __dirname + "/views"); // 디렉토리 설정
+app.use("/public", express.static(__dirname + "/public")); // public 폴더를 유저에게 공개 (유저가 볼 수 있는 폴더 지정)
+app.get("/", (req, res) => res.render("home")); // 홈페이지로 이동할 때 사용될 템플릿을 렌더
+app.get("/*", (req, res) => res.redirect("/")) // 홈페이지 내 어느 페이지에 접근해도 홈으로 연결되도록 리다이렉트 (다른 url 사용 안할거라)
+
+const hdlListen = () => console.log(`Listening on http://localhost:3000`)
+// app.listen(3000, hdlListen); // 3000번 포트와 연결
+
+const httpServer = http.createServer(app); // app은 requestlistener 경로 - express application으로부터 서버 생성
+const wsServer = new Server(httpServer, {
+    cors: {
+      origin: ["https://admin.socket.io"], // 이 URL에서 localhost:3000에 액세스할 것이기 때문에! - 온라인에서 Admin UI를 실제로 테스트할 수 있는 데모 사용을 위한 환경설정!
+      credentials: true
+    }
+}); // localhost:3000/socket.io/socket.io.js로 연결 가능 (socketIO는 websocket의 부가기능이 아니다!!)
+
+instrument(wsServer, {
+    auth: false, // 실제 비밀번호를 쓰도록 바꿀 수 있음!
+});
+
+
+function countRoom(g_room_name){ // 방에 사람이 몇명이 있는지 계산하는 함수(set의 size를 이용)
+    return wsServer.sockets.adapter.rooms.get(g_room_name)?.size; // g_room_name을 찾을 수도 있지만 못찾을 수도 있기 때문에 ?를 붙여준다
+}
+
+
+wsServer.on("connection", socket => {
+
+    socket.emit('io_system_msg', `socket.id : ${socket.id}`);
+
+    socket.onAny((event) => { // 미들웨어같은 존재! 어느 이벤트에서든지 console.log를 할 수 있다!
+        // console.log(wsServer.sockets.adapter); // 어댑터 동작 확인하기
+        console.log(`Socket Event:${event}`)
+        console.log(wsServer.sockets.adapter.rooms);
+    })
+
+    //로그인요청
+    socket.on("a000_login", (login_id) => {
+        // 1.로그인처리 (ToDo)
+        // 2.소켓닉네임등록
+        socket["nickname"] = login_id;
+        // 3.결과리턴
+        socket.emit('a000_login_result', '0', login_id);       //event, ret_code (0:성공), 로그인아이디
+    });
+
+    //로그아웃요청
+    socket.on("a999_logout", () => {
+        // 1.로그아웃처리 (ToDo)
+        // 2.결과리턴
+        socket.emit('a999_logout_result', '0', '로그아웃성공');  //event, ret_code (0:성공), 결과메시지
+    });
+    
+    //방생성요청
+    socket.on("b000_roomCreate", (room_name) => {
+        // 1.방생성처리 (ToDo)
+        // 2.방조인처리
+        socket.join(room_name);
+        console.log('room: ', wsServer.sockets.adapter.rooms);
+        // 3.결과리턴
+        socket.emit('b000_roomCreate_reuslt', '0', room_name);  //event, ret_code (0:성공), 결과메시지
+    });
+
+    //방나가기요청
+    socket.on("b999_roomLeave", (room_name) => {
+        // 1.방나가기처리 DB (ToDo)
+        // 2.방나기기처리
+        socket.leave(room_name);
+        // 3.결과리턴
+        socket.emit('b999_roomLeave_result', '0', '방나가기성공');  //event, ret_code (0:성공), 결과메시지
+        // 4.방메시지전송
+
+    });
+    
+    //메시지전송요청
+    socket.on("c000_msgSend", ( msg, room_name, login_id ) => { // 메세지랑 done 함수를 받을 것
+        console.log('c000_msgSend_return', login_id, room_name, msg)
+        //메시지전송
+        socket.to(room_name).emit('c000_msgSend_return', '0', `${login_id}: ${msg}`);
+        console.log('room: ', wsServer.sockets.adapter.rooms);
+        console.log(socket);
+    });
+    
+    
+});
+
+
+httpServer.listen(3000, hdlListen); // 서버는 ws, http 프로토콜 모두 이해할 수 있게 된다!
+
